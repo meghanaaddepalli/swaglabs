d763061018be9a23db160e44d845dcd90a9b0e8a
SONY VAIO <SONY VAIO@SONY>
2020-03-26 14:02:19 +0530

    final

--- src/test/java/com/pages/LoginPageM.java
+++ src/test/java/com/pages/LoginPageM.java
@@ -7,9 +7,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import cucumber.api.java.Before;
+
 public class LoginPageM {
 	WebDriver driver;
 
+	@Before
 	public void browser() {
 		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
 		driver = new ChromeDriver();
